75da5325f18e1d1d6eff277fc2961983bac42004
Leonardo Soto M <leo.soto@gmail.com>
2017-11-08 11:20:46 -0300

    Implement monitor endpoint

--- simple_example.rb
+++ simple_example.rb
@@ -5,3 +5,12 @@ get '/simple_example/hello' do
   content_type :json
   { respuesta: "Hola #{params[:name]}" }.to_json
 end
+
+get '/simple_example/monitor' do
+  content_type :json
+  {
+    codigo_estado: 200,
+    msj_estado: "OK",
+    desc_personalizada_estado: "Servicio opera correctamente"
+  }
+end
