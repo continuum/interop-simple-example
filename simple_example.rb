cddd332dd32fe25cc31dbe38fa3618c2c2bacf84
Eugenio <eugenionlopez@gmail.com>
2017-12-12 09:27:21 -0300

    Add mock logs and endpoint to serve them

--- simple_example.rb
+++ simple_example.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'json'
+BASE_PATH = File.expand_path(File.dirname(__FILE__)).freeze
 
 get '/simple_example/hello' do
   content_type :json
@@ -13,4 +14,15 @@ get '/simple_example/monitor' do
     msj_estado: "OK",
     desc_personalizada_estado: "Servicio opera correctamente"
   }.to_json
+end
+
+get '/simple_example/logs' do
+  start_date = params[:inicio]
+  end_date = params[:fin]
+  return send_file(BASE_PATH + '/mocks/mock_default.tar.gz',
+                   disposition: :attachment,
+                   filename: 'default.tar.gz') if start_date.nil? && end_date.nil?
+  send_file(BASE_PATH + '/mocks/mock_dos_dias.tar.gz',
+            disposition: :attachment,
+            filename: 'range.tar.gz')
 end
